e4d4a2b9da97d600361fda52be66c8619eab0d2a
Yahya1904 <135860509+Yahya1904@users.noreply.github.com>
2024-05-18 00:17:31 +0300

    switched to react

--- Website/scripts/load.js
+++
@@ -1,9 +0,0 @@
-const loading = document.getElementById('loading_div');
-
-setTimeout(function() {
-    loading.style.opacity = 0;
-}, 2000);
-setTimeout(function() {
-    loading.remove();
-    window.location.href = 'index.html';
-}, 3000);
