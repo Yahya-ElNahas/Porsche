626545d6cf4e4205d5f62293f2ae0b1813d7aaa0
youssefseyam212 <118624563+youssefseyam212@users.noreply.github.com>
2024-05-18 23:24:20 +0300

    Update EditUser.jsx
    ana wa7d bs
    ohohohoh

--- src/pages/EditUser.jsx
+++ src/pages/EditUser.jsx
@@ -5,14 +5,17 @@ import Cookies from 'js-cookie';
 import './styles/register.css';
 
 export default function EditUser() {
-    const [username, setUsername] = useState('');
+    const username = Cookies.get("username")
+    const type = Cookies.get('type')
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [mobileNumber, setMobileNumber] = useState('');
+    const [password, setPassword] = useState('');
 
     const navigate = useNavigate();
 
     useEffect(() => {
+        
         if(!Cookies.get('token') || !Cookies.get('type')) {
             navigate('/porsche')
             return
@@ -21,19 +24,22 @@ export default function EditUser() {
 
     const handleSubmit = async (event) => {
         event.preventDefault();
+
+        const updates = {};
+
+        if (firstName) updates.first_name = firstName;
+        if (lastName) updates.last_name = lastName;
+        if (mobileNumber) updates.mobile_numbers = [mobileNumber];
+        if (password) updates.password = password;
         
         try {
-            const response = await fetch(`http://localhost:3001/v1/api/Customers/${username}`, {
+            const response = await fetch(`http://localhost:3001/v1/api/Customers/:${username}`, {
                 method: 'PATCH',
                 headers: {
-                    'Content-Type': 'application/json'
+                    'Content-Type': 'application/json',
+                    'token':'' + Cookies.get("token")
                 },
-                body: JSON.stringify({
-                    username,
-                    first_name: firstName,
-                    last_name: lastName,
-                    mobile_numbers: [mobileNumber]
-                })
+                body: JSON.stringify(updates)
             });
 
             if (!response.ok) {
@@ -41,9 +47,11 @@ export default function EditUser() {
             }
 
             const data = await response.json();
+            console.log(type)
+            navigate("../profile")
            
         } catch (error) {
-            console.error('Error:', error);
+            console.log(error)            
         }
     };
 
@@ -57,13 +65,11 @@ export default function EditUser() {
                             <h1 className="hdr">Edit User</h1>
                             <form className="form" onSubmit={handleSubmit}>
                                 <div className="input-row">
-                                    <div className="input-group">
-                                        <label htmlFor="username">Username</label>
-                                        <input type="text" name="username" id="username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                                    </div>
                                 </div>
                                 <div className="input-row">
-                                    <div className="input-group">
+                                    {
+                                        type == 'Customers' &&(<>
+                                        <div className="input-group">
                                         <label htmlFor="first-name">First Name</label>
                                         <input type="text" name="first-name" id="first-name" className='lbox' value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                                     </div>
@@ -71,10 +77,15 @@ export default function EditUser() {
                                         <label htmlFor="last-name">Last Name</label>
                                         <input type="text" name="last-name" id="last-name" value={lastName} onChange={(e) => setLastName(e.target.value)} />
                                     </div>
-                                </div>
-                                <div className="input-group">
+                                    <div className="input-group">
                                     <label htmlFor="mobile-number">Mobile Number</label>
                                     <input type="tel" name="mobile-number" id="mobile-number" value={mobileNumber} onChange={(e) => setMobileNumber(e.target.value)} />
+                                </div></>)
+                                    }
+                                </div> 
+                                <div className="input-group">
+                                    <label htmlFor="password">Password</label>
+                                    <input type="tel" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                                 </div>
                                 <button type="submit" className="btn">Update</button>
                             </form>
